737b6eb54397d219ea07a2cfd47d82829846de95
TimothyMoralesLopez <222106895+TimothyMoralesLopez@users.noreply.github.com>
2025-08-06 19:14:41 -0400

    Finish JavaScript Exercise 07 ReverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function reverseStr(str) {
+    let strArray = str.split(''); 
+    strArray.reverse();
+    return strArray.join(''); 
 };
 
 // Do not edit below this line
