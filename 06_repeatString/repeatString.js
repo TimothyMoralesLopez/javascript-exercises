891e3d908a8a8639cb14f7cb9bbd8f48b8c41baf
TimothyMoralesLopez <222106895+TimothyMoralesLopez@users.noreply.github.com>
2025-08-06 18:55:28 -0400

    Finish JavaScript Exercise 06 RepeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = 
+function repeatStr(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        let repeatedStr = "";
+        for (let i = 0; i < num; i++) {
+            repeatedStr += str;
+        }
+        return repeatedStr;
+    }
 };
 
 // Do not edit below this line
