0b7a1b60a8fa4ca435080812253f388e26761bc6
TimothyMoralesLopez <222106895+TimothyMoralesLopez@users.noreply.github.com>
2025-08-06 20:17:37 -0400

    Finish JavaScript Exercise 08 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = 
+function removeElemsFromArray(arr, ...items) {
+
+    for (const item of items) {
+        if (arr.includes(item)) {
+            for (let i = 0; i < arr.length; i++)
+            {
+                if (arr[i] == item) {
+                    arr.splice(i, 1); 
+                    i--; 
+                }
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
