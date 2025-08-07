62a27c9866788ad7db71e592cb6a3ec66885d388
TimothyMoralesLopez <222106895+TimothyMoralesLopez@users.noreply.github.com>
2025-08-06 20:30:56 -0400

    Finish JavaScript Exercise 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = 
+function getSum(a, b) {
+    
+    let finalSum = 0; 
 
+    if (Math.abs(Math.floor(a)) !== a || Math.abs(Math.floor(b)) !== b) {
+        return "ERROR"; 
+    }
+
+    if (a > b) {
+        let tempNum = a;
+        a = b;
+        b = tempNum; 
+    }
+
+    for (let i = a; i < b + 1; i++) {
+        finalSum += i;
+    } 
+
+    return finalSum; 
 };
 
 // Do not edit below this line
